48df75a5473eb3784c0fbc6778f3d9ed8d6f37d2
caohongd <amesatchu2014@gmail.com>
2017-05-21 16:02:25 -0700

    Added routes

---
+++ src/app/app-route.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from 'app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+
+const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: '/cut', component: WelcomeComponent },
+    { path: '/recipe/:recipeId', component: RecipeDetailsComponent}
+];
+
+export const routing = RouterModule.forRoot(routes);
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+//import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
+import { APP_BASE_HREF } from '@angular/common';
+import { routing } from './app-route';
+
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    RecipeDetailsComponent
+   RecipeDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
   ],
-  providers: [],
+  providers: [ {provide: APP_BASE_HREF, useValue: '/'}, SocialService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
