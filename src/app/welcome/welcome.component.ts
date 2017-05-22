1f02338aec480f9341152d8a1082d0f48bcf56fe
caohongd <amesatchu2014@gmail.com>
2017-05-22 11:49:17 -0700

    edits

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialService } from '../social.service';
+import {Router, Params } from '@angular/router';
+
 
 @Component({
   selector: 'app-welcome',
@@ -7,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  recipeTitle: string;
+  recipeInfo: string;
+  social$: SocialService;
+  route$: Router;
+
+  constructor(
+    _social: SocialService,
+    _route: Router,
+  ) {
+    this.social$ = _social;
+    this.route$= _route;
+   }
 
   ngOnInit() {
   }
